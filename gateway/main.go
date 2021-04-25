60922ed8e5a04a5cc0560343f1ab9f90dd66cf7c
BusinessL <1138326692@qq.com>
2021-04-25 16:33:34 +0800

    增加gateway 网关

--- gateway/main.go
+++ gateway/main.go
@@ -1,1 +1,49 @@
-package gateway
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/encoding/protojson"
+
+	authpb "pusher/auth/api/gen/v1"
+)
+
+func main() {
+	c := context.Background()
+	c, cancel := context.WithCancel(c)
+	defer cancel()
+
+	mux := runtime.NewServeMux(runtime.WithMarshalerOption(
+		runtime.MIMEWildcard,
+		&runtime.JSONPb{
+			MarshalOptions: protojson.MarshalOptions{
+				UseEnumNumbers: true, // 枚举字段的值使用数字
+				UseProtoNames:  true,
+				// 传给 clients 的 json key 使用下划线 `_`
+				// AccessToken string `protobuf:"bytes,1,opt,name=access_token,json=accessToken,proto3" json:"access_token,omitempty"`
+				// 这里说明应使用 access_token
+			},
+			UnmarshalOptions: protojson.UnmarshalOptions{
+				DiscardUnknown: true, // 忽略 client 发送的不存在的 poroto 字段
+			},
+		},
+	))
+	err := authpb.RegisterAuthServiceHandlerFromEndpoint(
+		c,
+		mux,
+		"localhost:8081",
+		[]grpc.DialOption{grpc.WithInsecure()},
+	)
+	if err != nil {
+		log.Fatalf("cannot register auth service: %v", err)
+	}
+
+	err = http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatalf("cannot listen and server: %v", err)
+	}
+}
