99c4a5967b38bf2f969621b5812ac9385d67884e
BusinessL <1138326692@qq.com>
2021-04-30 21:54:40 +0800

    email

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.14
 require (
 	github.com/go-redis/redis/v8 v8.8.2
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.3.0
-	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
 	github.com/medivhzhan/weapp/v2 v2.4.1
 	golang.org/x/net v0.0.0-20210420210106-798c2154c571 // indirect
 	golang.org/x/sys v0.0.0-20210420205809-ac73e9fd8988 // indirect
--- service/email/email.go
+++ service/email/email.go
@@ -2,22 +2,67 @@ package email
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"net/mail"
 	"net/smtp"
 	emailpb "pusher/proto/email"
-
-	emailTool "github.com/jordan-wright/email"
+	"strings"
 )
 
 type Server struct {
 	emailpb.UnimplementedEmailServiceServer
 }
 
+func encodeRFC2047(String string) string {
+	// use mail's rfc2047 to encode any string
+	addr := mail.Address{String, ""}
+	return strings.Trim(addr.String(), " <>")
+}
+
 func (s *Server) Send(ctx context.Context, req *emailpb.EmailRequest) (*emailpb.EmailResponse, error) {
-	e := emailTool.NewEmail()
-	e.From = "Jordan "
-	e.To = []string{"max.li@madv360.com"}
-	e.Text = []byte("Text body is")
-	e.Send("smtp.gmail.com:587", smtp.PlainAuth("", "test@gmail.com", "password123", "smtp.gmail.com"))
+	smtpServer := "smtp.exmail.qq.com"
+	auth := smtp.PlainAuth(
+		"",
+		"max.li@madv.com",
+		"yYQiUY8DGi",
+		smtpServer,
+	)
+
+	from := mail.Address{"监控中心", "max.li@madv.com"}
+	to := mail.Address{"收件人", "113832@qq.com"}
+	title := "当前时段统计报表"
+
+	body := "报表内容一切正常"
+
+	header := make(map[string]string)
+	header["From"] = from.String()
+	header["To"] = to.String()
+	header["Subject"] = encodeRFC2047(title)
+	header["MIME-Version"] = "1.0"
+	header["Content-Type"] = "text/plain; charset=\"utf-8\""
+	header["Content-Transfer-Encoding"] = "base64"
+
+	message := ""
+	for k, v := range header {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	message += "\r\n" + base64.StdEncoding.EncodeToString([]byte(body))
+
+	// Connect to the server, authenticate, set the sender and recipient,
+	// and send the email all in one step.
+	err := smtp.SendMail(
+		smtpServer+":587",
+		auth,
+		from.Address,
+		[]string{to.Address},
+		[]byte(message),
+		//[]byte("This is the email body."),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	return &emailpb.EmailResponse{Code: "success"}, nil
 }
